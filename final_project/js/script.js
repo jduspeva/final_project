0270ba1f71c27cdad44309c4c3d0bf077ecf687c
jduspeva <79641730+jduspeva@users.noreply.github.com>
2023-04-18 22:38:49 -0400

    Add files via upload

--- final_project/js/script.js
+++ final_project/js/script.js
@@ -1,17 +1,17 @@
-import Lightbox from './js/photoswipe-lightbox.esm.js';
-import PhotoSwipe from './js/photoswipe.esm.js';
+import Lightbox from '../js/photoswipe-lightbox.esm';
+import PhotoSwipe from '../js/photoswipe.esm.js';
 const lightbox1 = new Lightbox({
     gallery: '#port_arch',
     children: 'a',
     pswpModule: PhotoSwipe
 });
 const lightbox2 = new Lightbox({
-    gallery: '#port_arch',
+    gallery: '#port_auto',
     children: 'a',
     pswpModule: PhotoSwipe
 });
 const lightbox3 = new Lightbox({
-    gallery: '#port_arch',
+    gallery: '#port_port',
     children: 'a',
     pswpModule: PhotoSwipe
 });
